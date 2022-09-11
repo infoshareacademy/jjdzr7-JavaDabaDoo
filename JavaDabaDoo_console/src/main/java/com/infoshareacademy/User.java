cd27e4f652aae4f85593c5e790457447a7eb05c5
Krzysztof <k.mazur24@gmail.com>
2022-09-11 19:19:55 +0200

    Update User class

--- JavaDabaDoo_console/src/main/java/com/infoshareacademy/User.java
+++ JavaDabaDoo_console/src/main/java/com/infoshareacademy/User.java
@@ -1,22 +1,62 @@
+package com.infoshareacademy;
+
+import com.infoshareacademy.Book;
+
 import java.util.ArrayList;
 import java.util.List;
 
 public class User {
-    public int UserId;
-    public String firstName;
-    public String lastName;
-    private ArrayList<String> borrowlist;
+    private int userId;
+    private String firstName;
+    private String lastName;
+    private List<Book> borrowlist;
+    private List<Book> history;
 
-    public String getFullName() {
-        return firstName + lastName;
+    public User(int userId, String firstName, String lastName) {
+        this.userId = userId;
+        this.firstName = firstName;
+        this.lastName = lastName;
+        this.borrowlist = new ArrayList<>();
+        this.history = new ArrayList<>();
     }
-    public ArrayList<String> getBorrowlistlist(){
+
+    public int getUserId() {
+        return userId;
+    }
+
+    public void setUserId(int userId) {
+        this.userId = userId;
+    }
+
+    public String getFirstName() {
+        return firstName;
+    }
+
+    public void setFirstName(String firstName) {
+        this.firstName = firstName;
+    }
+
+    public String getLastName() {
+        return lastName;
+    }
+
+    public void setLastName(String lastName) {
+        this.lastName = lastName;
+    }
+
+    public List<Book> getBorrowlist() {
         return borrowlist;
     }
-    List<String> lista;
 
-    {
-        lista = new ArrayList<String>();
+    public void setBorrowlist(List<Book> borrowlist) {
+        this.borrowlist = borrowlist;
     }
- 		lista.add("ksiazka");
+
+    public List<Book> getHistory() {
+        return history;
+    }
+
+    public void setHistory(List<Book> history) {
+        this.history = history;
+    }
 }
