39f8d3d9109f2ff2953595d2839f9c8dd3d652bc
Michal Mrozek <m.mrozek@hotmail.com>
2022-09-14 17:11:54 +0100

    changes

--- JavaDabaDoo_console/src/main/java/com/infoshareacademy/Main.java
+++ JavaDabaDoo_console/src/main/java/com/infoshareacademy/Main.java
@@ -2,6 +2,9 @@ package com.infoshareacademy;
 
 public class Main {
     public static void main(String[] args) {
+
         System.out.println("Hello World!");
+        LibraryControl library = new LibraryControl();
+        library.controlLoop();
     }
 }
--- JavaDabaDoo_console/src/main/java/com/infoshareacademy/User.java
+++ JavaDabaDoo_console/src/main/java/com/infoshareacademy/User.java
@@ -1,6 +1,6 @@
 package com.infoshareacademy;
 
-import com.infoshareacademy.Book;
+
 
 import java.util.ArrayList;
 import java.util.List;
