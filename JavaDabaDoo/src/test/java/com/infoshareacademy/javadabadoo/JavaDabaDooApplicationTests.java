4f56256e52b6dafc22db986d6ba37d7697088eb4
drlf <darullef4@gmail.com>
2022-10-04 20:52:05 +0200

    new project

---
+++ JavaDabaDoo/src/test/java/com/infoshareacademy/javadabadoo/JavaDabaDooApplicationTests.java
@@ -0,0 +1,13 @@
+package com.infoshareacademy.javadabadoo;
+
+import org.junit.jupiter.api.Test;
+import org.springframework.boot.test.context.SpringBootTest;
+
+@SpringBootTest
+class JavaDabaDooApplicationTests {
+
+    @Test
+    void contextLoads() {
+    }
+
+}
