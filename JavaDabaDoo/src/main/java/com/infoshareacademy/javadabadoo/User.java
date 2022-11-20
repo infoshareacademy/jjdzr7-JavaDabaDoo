8b72ca45ec8bf28752fd88d6ab9723380496cb4c
Grabowski-Jakub <grabowskija@gmail.com>
2022-11-20 18:48:16 +0100

    Before Merge

--- JavaDabaDoo/src/main/java/com/infoshareacademy/javadabadoo/User.java
+++ JavaDabaDoo/src/main/java/com/infoshareacademy/javadabadoo/User.java
@@ -1,35 +1,6 @@
 package com.infoshareacademy.javadabadoo;
 
-import java.util.Objects;
-
 public class User {
-    public String UnecceserryTextField = "Remove this line";
+    
 
-    public User(String unecceserryTextField) {
-        UnecceserryTextField = unecceserryTextField;
-    }
-
-    public User() {
-    }
-
-    public String getUnecceserryTextField() {
-        return UnecceserryTextField;
-    }
-
-    public void setUnecceserryTextField(String unecceserryTextField) {
-        UnecceserryTextField = unecceserryTextField;
-    }
-
-    @Override
-    public boolean equals(Object o) {
-        if (this == o) return true;
-        if (o == null || getClass() != o.getClass()) return false;
-        User user = (User) o;
-        return Objects.equals(UnecceserryTextField, user.UnecceserryTextField);
-    }
-
-    @Override
-    public int hashCode() {
-        return Objects.hash(UnecceserryTextField);
-    }
 }
