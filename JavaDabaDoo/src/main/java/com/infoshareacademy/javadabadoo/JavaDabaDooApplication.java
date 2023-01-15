ff948065a7724669b7612b4fe3d98433fe42cc1d
Grzegorz Przebinda <grzegorz.przebinda@gmail.com>
2023-01-15 19:58:28 +0100

    add properties for jpa

--- JavaDabaDoo/src/main/java/com/infoshareacademy/javadabadoo/JavaDabaDooApplication.java
+++ JavaDabaDoo/src/main/java/com/infoshareacademy/javadabadoo/JavaDabaDooApplication.java
@@ -1,7 +1,5 @@
 package com.infoshareacademy.javadabadoo;
 
-import com.infoshareacademy.javadabadoo.repository.ItemFileHandler;
-import com.infoshareacademy.javadabadoo.repository.UsersFileHandler;
 import org.springframework.boot.SpringApplication;
 import org.springframework.boot.autoconfigure.SpringBootApplication;
 
@@ -11,13 +9,12 @@ public class JavaDabaDooApplication {
     public static void main(String[] args) {
         SpringApplication.run(JavaDabaDooApplication.class, args);
 
-        ItemFileHandler file = new ItemFileHandler();
-        var list = file.read();
-        System.out.println(list);
-
-        UsersFileHandler user = new UsersFileHandler();
-        var users = user.readUsers();
-        System.out.println(users);
+//        ItemFileHandler file = new ItemFileHandler();
+//        var list = file.read();
+//        System.out.println(list);
+//
+//        UserFileHandler user = new UserFileHandler();
+//        System.out.println(user.readUser());
     }
 
 }
