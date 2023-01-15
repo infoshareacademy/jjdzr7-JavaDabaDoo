5968bb5df3768d7aaa9d7749aacdf0e0a0f5bf0d
Grabowski-Jakub <grabowskija@gmail.com>
2023-01-15 19:37:53 +0100

    [task] some fixes

--- JavaDabaDoo/src/test/java/com/infoshareacademy/javadabadoo/JavaDabaDooApplicationTests.java
+++ JavaDabaDoo/src/test/java/com/infoshareacademy/javadabadoo/JavaDabaDooApplicationTests.java
@@ -1,14 +1,18 @@
 package com.infoshareacademy.javadabadoo;
 
 import com.fasterxml.jackson.databind.ObjectMapper;
+import com.infoshareacademy.javadabadoo.model.Book;
+import com.infoshareacademy.javadabadoo.model.Item;
+import com.infoshareacademy.javadabadoo.model.Language;
 import com.infoshareacademy.javadabadoo.model.User;
 import com.infoshareacademy.javadabadoo.repository.IUserProvider;
-import com.infoshareacademy.javadabadoo.repository.UserFileHandler;
+import com.infoshareacademy.javadabadoo.repository.UsersFileHandler;
 import org.junit.jupiter.api.Test;
 import org.springframework.beans.factory.annotation.Autowired;
 import org.springframework.boot.test.context.SpringBootTest;
 import org.springframework.core.io.ResourceLoader;
 
+import java.util.ArrayList;
 import java.util.List;
 
 @SpringBootTest
@@ -21,15 +25,24 @@ class JavaDabaDooApplicationTests {
     ResourceLoader resourceLoader;
 
     @Test
-    void serializeAndDeserializeUserTest() {
+    void serializeAndDeserializeUser() {
         // to be removed
         IUserProvider test = new UserFileHandler(mapper, resourceLoader);
         User user = new User(1, "test", "test");
-        List<Integer> history = List.of(1, 2, 3);
-        List<Integer> borrowed = List.of(4, 5, 6);
+        test.saveUser(user);
+        List<Item> history = List.of(new Book(1, "abc javy", "Nikt wazny", Language.POLISH, "aaaaaaaaa", "lektura"));
+        List<Item> borrowed = List.of(new Book(2, "abc javy i nic", "Nikt wazny", Language.POLISH, "aaaaaaaaa", "lektura"));
         user.setBorrowlist(borrowed);
         user.setHistory(history);
-        test.saveUser(user);
+        User user2 = new User(1, "testt", "testt");
+        List<Integer> history2 = List.of(1, 2, 3);
+        List<Integer> borrowed2 = List.of(4, 5, 6);
+        user2.setBorrowlist(borrowed2);
+        user2.setHistory(history2);
+        List<User> userList = new ArrayList<>();
+        userList.add(user);
+        userList.add(user2);
+        test.saveUsers(userList);
 
         User user2 = test.readUser();
         System.out.println(user.toString());
