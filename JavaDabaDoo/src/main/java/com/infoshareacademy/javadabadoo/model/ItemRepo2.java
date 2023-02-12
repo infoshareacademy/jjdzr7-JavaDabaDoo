ebfcb88505fb8f76bd2d56493f580e322e726d7a
Michal Mrozek <m.mrozek@hotmail.com>
2023-02-12 17:39:53 +0000

    Relations sorted - files removed.

--- JavaDabaDoo/src/main/java/com/infoshareacademy/javadabadoo/model/ItemRepo2.java
+++
@@ -1,8 +0,0 @@
-package com.infoshareacademy.javadabadoo.model;
-
-import org.springframework.data.jpa.repository.JpaRepository;
-import org.springframework.stereotype.Repository;
-
-@Repository
-public interface ItemRepo2 extends JpaRepository<Item, Long> {
-}
