1bfdf66bcf990f0e3b23b7395d097735ccfac0d4
Grabowski-Jakub <grabowskija@gmail.com>
2023-03-11 12:27:21 +0100

    removing commeted code

--- JavaDabaDoo/src/main/java/com/infoshareacademy/javadabadoo/JavaDabaDooApplication.java
+++ JavaDabaDoo/src/main/java/com/infoshareacademy/javadabadoo/JavaDabaDooApplication.java
@@ -9,12 +9,6 @@ public class JavaDabaDooApplication {
 
     public static void main(String[] args) {
         ConfigurableApplicationContext run = SpringApplication.run(JavaDabaDooApplication.class, args);
-//        BookRepository bookr = run.getBean(BookRepository.class);
-//        AudioBookRepository audior = run.getBean(AudioBookRepository.class);
-//        AudioBook audio = new AudioBook("title", "autor", Language.ENGLISH,"mp3", "WOjew", 233l);
-//        Book book = new Book("title", "autor", Language.POLISH, "123456", "fantasy");
-//        bookr.save(book);
-//        audior.save(audio);
 
     }
 
